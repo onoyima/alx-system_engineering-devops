46d1793ad1a419f2111cb7a59bed7e4e529287f5
onoyima <clintonfaze@outlook.com>
2024-06-05 13:45:51 +0000

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+
 """
 0-subs.py
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+"""
+Returns the number of subscribers for a given subreddit.
+"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
