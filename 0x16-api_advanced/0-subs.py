b39e6b38fdec42f8b2ba051feb85f330f7b87989
onoyima <clintonfaze@outlook.com>
2024-06-06 11:08:52 -0700

    Advanced project correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Module to query a number of subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Return the total number of subscribers on a given subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    # Print "OK" for any response
-    print("OK")
-
-    if response.status_code != 200:
-        return 0
-
     try:
-        data = response.json()
-        print(data)  # Print JSON response for inspection
-        if "data" in data and "subscribers" in data["data"]:
-            return data["data"]["subscribers"]
-        else:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 404:
             return 0
-    except ValueError:
+        results = response.json().get("data")
+        return results.get("subscribers", 0)
+    except Exception:
         return 0
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
