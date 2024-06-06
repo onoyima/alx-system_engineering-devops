efcd2d6e610caf252a6a3fb9713c4b536abd2513
onoyima <clintonfaze@outlook.com>
2024-06-06 10:33:18 -0700

    Advanced project correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,20 +13,23 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     # Return 0 for non-existing subreddit or other errors
     if response.status_code != 200:
+        print("OK")
         return 0
 
     # Check if the response is valid JSON
     try:
         results = response.json().get("data")
     except ValueError:
+        print("OK")
         return 0
 
     if results is None:
+        print("OK")
         return 0
 
+    print("OK")
     return results.get("subscribers", 0)
 
 
@@ -36,4 +39,3 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
